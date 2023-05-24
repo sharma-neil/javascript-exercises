4e81272456713c1240f37af2e73c1af99002554d
sharma-neil <ns86.sharma@gmail.com>
2023-05-23 21:52:28 -0400

    01 to 03 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str_val,rep_val) {
+    if (rep_val < 0) return 'ERROR';
+    let word = ''
+    for (let i = 0; i < rep_val; i++) {
+        word += str_val;
+    }
+    return word;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    rev_word = '';
+
+    for (let i = word.length - 1; i >= 0; i--){
+        rev_word += word[i];
+    }
+
+    return rev_word;
 };
 
 // Do not edit below this line
